ee4fd3c73fc51661350d4be40e42d2aec7c23e48
i-ii-iii-iiii-v <ksuk1996@gmail.com>
2018-10-11 18:37:58 -0700

    shell argument parsing done, may have bugs

--- OS/projects/shell/shell.c
+++ OS/projects/shell/shell.c
@@ -1,29 +1,53 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdbool.h>
 #define MAX_LINE 80
 #define BUFFER_SIZE 1024
-
+#define MAX_ARG 40
+#define MAX_ARG_LENGTH 128
 int main(int argc, char *argv[])
 {
-    char *arguments[MAX_LINE / 2 + 1];
 	char buffer[BUFFER_SIZE];
+	char arg_array[MAX_ARG][MAX_ARG_LENGTH];
     // compiler should not optimize this variable
     volatile bool run = true;
 
-    while(run)
+    while (run)
     {
-        fprintf(stdout, "osh>");
         fflush(stdout);
+        fprintf(stdout, "\nosh>");
+
+		size_t arg_count = 0;
 
         // Read user input
-		while((fgets(buffer, BUFFER_SIZE, stdin))
+		while (fgets(buffer, BUFFER_SIZE - 1, stdin))
 		{
-			// Parse arguments
+			const char * argument = buffer;
+			// Parse into array
+			// Malloc every arg? or is it inefficient?
+			while (sscanf(argument, "%s", arg_array[arg_count]) != EOF) 
+			{
+				arg_count ++;
+				// Just ignore the rest of the arg
+				if (arg_count >= MAX_ARG)
+					break;
+		
+				// No more arguments
+				argument = strchr(argument, ' ');
+				if (argument == NULL || ++argument == NULL) 
+					break;
+			}
 
+			for (size_t i = 0; i < arg_count; i++) 
+				printf("%s ", arg_array[i]);
+
+			// User pressed enter; Loop exit condition 
+			if (strchr(buffer, '\n')) 
+				break;
 		}
-        // Fork child process
 
+        // Fork child process
         // Child invoke exec
 
         // parent will invoke wait() unless command included &
